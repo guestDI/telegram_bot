d970b4b16f08a35a600e087764304c80aeba782c
Dzmitry <ignatovich.dm@gmail.com>
2024-10-27 22:49:08 +0100

    fix issue with recurring poll

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const cron = require('node-cron');
 const { getNextEventTime } = require('./utils')
+const moment = require('moment-timezone');
 
 const token = '7630370286:AAGz6H5lbpJ1xDAqDkIV6f_NJcsQgmNMbtQ';
 const bot = new TelegramBot(token, { polling: true });
@@ -11,6 +12,7 @@ let eventPollId = null;
 let manOfTheMatchPollId = null;
 let eventPollAnswers = [];
 let scheduledTask = null;
+const pollTasks = {};
 
 // Schedule a poll every Monday at 11:00 AM (server time)
 cron.schedule('0 0 11 * * 1', () => {
@@ -27,7 +29,7 @@ cron.schedule('0 0 21 * * 2', () => {
     }
   });
 
-  bot.onText(/\/start/, (msg) => {
+  bot.onText('start', (msg) => {
     const chatId = msg.chat.id;
     
     const inlineMenu = {
@@ -35,7 +37,7 @@ cron.schedule('0 0 21 * * 2', () => {
         inline_keyboard: [
           [
             { text: 'Recurring poll', callback_data: 'schedule_recurring_poll' },
-            { text: 'Event poll', callback_data: 'start_event_poll' },
+            { text: 'Event poll', callback_data: 'send_event_poll' },
             { text: 'Man of the match', callback_data: 'man_of_the_match' },
           ],
           [
@@ -52,10 +54,12 @@ cron.schedule('0 0 21 * * 2', () => {
     const chatId = query.message.chat.id;
     const userId = query.from.id;
   
-    if (query.data === 'start_event_poll') {
+    if (query.data === 'schedule_recurring_poll') {
+        scheduleRecurringPoll(query.message)
+        bot.answerCallbackQuery(query.id, { text: 'Recurring poll is set!' });
+      } else if (query.data === 'send_event_poll') {
       const eventTime = getNextEventTime();
       sendEventPoll(chatId, eventTime);
-      bot.answerCallbackQuery(query.id, { text: 'Опрос на событие запущен!' });
     } else if (query.data === 'man_of_the_match') {
       const eventTime = getNextEventTime();
       sendManOfTheMatchPoll(chatId, eventTime);
@@ -63,7 +67,6 @@ cron.schedule('0 0 21 * * 2', () => {
     } else if (query.data === 'random_user') {
       const randomUserId = chatMembers[Math.floor(Math.random() * chatMembers.length)];
       bot.sendMessage(chatId, `Составы определяет: <a href="tg://user?id=${randomUserId}">${randomUserId}</a>`, { parse_mode: 'HTML' });
-      bot.answerCallbackQuery(query.id, { text: 'Случайный пользователь выбран!' });
     }
   });
 
@@ -94,6 +97,73 @@ function sendManOfTheMatchPoll(chatId, eventTime) {
       });
   }
 
+function scheduleRecurringPoll(msg){
+    const chatId = msg.chat.id;
+  
+    bot.sendMessage(chatId, "Please enter day, time and number of occurencies (optional) in the following format:\n\n*day (from 0 to 6) time [num of occurencies]*\n\nExample: `0 15:00 10` or `3 18:30` where 0 is Sunday and 3 is Wednesday ", { parse_mode: 'Markdown' });
+    
+    bot.once('message', (dateMsg) => {
+      const input = dateMsg.text.split(' ');
+      if (input.length < 2) {
+        bot.sendMessage(chatId, "Wrong format. Please, enter day (0-6) and time.");
+        return;
+      }
+  
+      const day = input[0].toLowerCase();
+      const time = input[1];
+      const repeatCount = input[2] ? parseInt(input[2], 10) : Infinity;
+      const dayMapping = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+      if (Number(day) == NaN || Number(day) > 6 || Number(day) < 0 || !moment(time, 'HH:mm', true).isValid()) {
+        bot.sendMessage(chatId, "Wrond format for a day or time. Please try again.");
+        
+        return;
+      }
+  
+      const [hour, minute] = time.split(':').map(Number);
+      const cronExpression = `0 ${minute} ${hour} * * ${day}`;
+  
+      let count = 0;
+      pollTasks[chatId] = cron.schedule(cronExpression, () => {
+        if (count >= repeatCount) {
+          pollTasks[chatId].stop();
+          bot.sendMessage(chatId, "Recurring day is finished.");
+          return;
+        }
+        count++;
+        sendRecurringPoll(chatId, day, time);
+      });
+  
+      bot.sendMessage(chatId, `Recurring poll is scheduled for ${dayMapping[day]} at ${time}. ${repeatCount === Infinity ? 'Poll doesn\'t have finish date.' : `Number of occurencies: ${repeatCount}.`}`);
+    });
+}  
+
+function sendRecurringPoll(chatId, day, time) {
+    const eventTime = moment().day(day).hour(parseInt(time.split(':')[0])).minute(parseInt(time.split(':')[1]));
+    const eventDateString = eventTime.format('dddd, MMMM Do YYYY, HH:mm');
+    
+    bot.sendPoll(chatId, `Football Event: ${eventDateString}`, ['Yes', 'No'], { is_anonymous: false })
+      .then(() => {
+        console.log(`Recurring poll sent to chat ${chatId}`);
+      })
+      .catch((err) => {
+        console.error(`Failed to send recurring poll to chat ${chatId}:`, err);
+      });
+  }
+  
+  // Команда для отмены регулярного опроса
+  bot.onText(/\/cancel/, (msg) => {
+    const chatId = msg.chat.id;
+    
+    if (pollTasks[chatId]) {
+      pollTasks[chatId].stop();
+      delete pollTasks[chatId];
+      bot.sendMessage(chatId, "Регулярный опрос отменен.");
+    } else {
+      bot.sendMessage(chatId, "Нет активных регулярных опросов для отмены.");
+    }
+  });
+
 bot.on('poll_answer', (pollAnswer) => {
     const pollId = pollAnswer.poll_id;
     const userId = pollAnswer.user.id;
@@ -117,7 +187,7 @@ bot.on('message', (msg) => {
   }
 
   // Command to manually start a poll
-  if (msg.text === '/startpoll') {
+  if (msg.text === '/sendpoll') {
     const eventTime = getNextEventTime();
     sendEventPoll(chatId, eventTime);
   }
@@ -128,27 +198,7 @@ bot.on('message', (msg) => {
   }
 
   if(msg.text === '/schedule_recurring_poll') {
-    bot.sendMessage(chatId, "Please send me the day and time for scheduling the event in format 'YYYY-MM-DD HH:mm'");
-  
-    bot.once('message', (dateMsg) => {
-      const inputDate = moment(dateMsg.text, 'YYYY-MM-DD HH:mm', true);
-  
-      if (!inputDate.isValid()) {
-        bot.sendMessage(chatId, "Invalid date format. Please use 'YYYY-MM-DD HH:mm'.");
-        return;
-      }
-  
-      const date = inputDate.toDate();
-  
-      const cronExpression = `0 ${date.getMinutes()} ${date.getHours()} * * ${date.getDay()}`;
-  
-      if (scheduledTask) scheduledTask.destroy(); 
-      scheduledTask = cron.schedule(cronExpression, () => {
-        sendEventPoll(chatId, inputDate); 
-      });
-  
-      bot.sendMessage(chatId, `Event scheduled for ${inputDate.format('dddd, MMMM Do YYYY, HH:mm')}`);
-    });
+    scheduleRecurringPoll(msg)
   }
 });
 
